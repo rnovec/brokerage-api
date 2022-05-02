c58cf2c672b1f93243c5e8edf91835440f92b20d
Raul Novelo <rnovelo.cruz98@gmail.com>
2022-05-02 17:22:14 -0500

    Add testing and implment account creation - closes #1

--- app/api/controllers.py
+++ app/api/controllers.py
@@ -1,1 +1,14 @@
+from sqlalchemy.orm import Session
 
+from app.database.models import Account
+
+from .schemas import AccountSchema
+
+
+def create_account(db: Session, payload: AccountSchema):
+    """Create a new investment account."""
+    account = Account(cash=payload.cash)
+    db.add(account)
+    db.commit()
+    db.refresh(account)
+    return account
