5580999bfa0b6036f155e20e3415fe3fbb69eded
Raul Novelo <rnovelo.cruz98@gmail.com>
2022-05-02 20:45:09 -0500

    Add testing and implementation for orders creation - closes #2

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List
 
 from pydantic import BaseModel
 
@@ -14,3 +14,30 @@ class AccountSchemaRead(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class OrderSchema(BaseModel):
+    timestamp: float
+    operation: str
+    issuer_name: str
+    total_shares: int
+    shared_price: float
+
+
+class OrderSchemaItem(BaseModel):
+    issuer_name: str
+    total_shares: int
+    shared_price: float
+
+    class Config:
+        orm_mode = True
+
+
+class BalanceSchema(BaseModel):
+    cash: float
+    issuers: List[OrderSchemaItem] = []
+
+
+class OperationSchemaResponse(BaseModel):
+    current_balance: BalanceSchema
+    business_errors: List[str] = []
